2c69bc2ab973bc644e04fbda554e2ac77cef74e2
Lars Borisek <lars.borisek002@stud.fh-dortmund.de>
2018-12-04 16:53:22 +0100

    Added new App-Client project

---
+++ FortDay-app-client/src/de/fh_dortmund/cw/surstwalat/client/Main.java
@@ -0,0 +1,15 @@
+package de.fh_dortmund.cw.surstwalat.client;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+	}
+
+}
