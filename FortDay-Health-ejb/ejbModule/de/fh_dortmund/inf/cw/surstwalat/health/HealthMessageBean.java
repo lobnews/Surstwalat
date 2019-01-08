3470d27dffcc7e8502e891b5f95fb357418f44aa
Mankianer <Marvin.woelk@web.de>
2019-01-08 15:58:58 +0100

    Resolved merge conflict by incorporating both suggestions.

---
+++ FortDay-Health-ejb/ejbModule/de/fh_dortmund/inf/cw/surstwalat/health/HealthMessageBean.java
@@ -0,0 +1,27 @@
+package de.fh_dortmund.inf.cw.surstwalat.health;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+/**
+ * Message-Driven Bean implementation class for: HealthMessageBean
+ */
+@MessageDriven(
+        activationConfig = {
+            @ActivationConfigProperty(
+                    propertyName = "destinationType", propertyValue = "javax.jms.Topic")
+        },
+        mappedName = "java:global/jms/HealthMessageTopic")
+public class HealthMessageBean implements MessageListener {
+
+    /**
+     * @see MessageListener#onMessage(Message)
+     */
+    public void onMessage(Message message) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
