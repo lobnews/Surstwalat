b455aa93b010241577187d9f093aa3f9bc28c804
dejheid <johannes.heiderich@vanderlande.com>
2019-01-08 15:50:13 +0100

    Common: Changed MessageType and PropertyType to classes with String constants

--- FortDay-Dispatcher-ejb/ejbModule/de/fh_dortmund/inf/cw/surstwalat/dispatcher/beans/events/eventhandler/GameStartedEventHandlerBean.java
+++ FortDay-Dispatcher-ejb/ejbModule/de/fh_dortmund/inf/cw/surstwalat/dispatcher/beans/events/eventhandler/GameStartedEventHandlerBean.java
@@ -1,12 +1,14 @@
 package de.fh_dortmund.inf.cw.surstwalat.dispatcher.beans.events.eventhandler;
 
 import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
 import de.fh_dortmund.inf.cw.surstwalat.common.MessageType;
 import de.fh_dortmund.inf.cw.surstwalat.common.PropertyType;
+import de.fh_dortmund.inf.cw.surstwalat.dispatcher.interfaces.DispatcherLocal;
 
 /**
  * Message-Driven Bean implementation class for: GameStartedEventHandlerBean
@@ -22,12 +24,14 @@ import de.fh_dortmund.inf.cw.surstwalat.common.PropertyType;
 public class GameStartedEventHandlerBean implements MessageListener {
 
 	
+	@EJB
+	private DispatcherLocal dispatcher;
 	/**
      * @see MessageListener#onMessage(Message)
      */
     public void onMessage(Message message) {
-        // TODO Auto-generated method stub
-        
+        // TODO Extract attributes from Message
+        // dispatcher.addGame(gameId, kiCount, userIds);
     }
 
 }
--- FortDay-common/src/de/fh_dortmund/inf/cw/surstwalat/common/MessageType.java
+++ FortDay-common/src/de/fh_dortmund/inf/cw/surstwalat/common/MessageType.java
@@ -1,11 +1,11 @@
 package de.fh_dortmund.inf.cw.surstwalat.common;
 
-public interface MessageType {
-	public static final int ASSIGN_ACTIVE_PLAYER = 0;
-	public static final int ASSIGN_PLAYER = 1;
-	public static final int ELIMINATE_PLAYER = 2;
-	public static final int END_ROUND = 3;
-	public static final int PLAYER_ROLL = 4;
-	public static final int SPAWN_ITEM = 5;
-	public static final int START_ROUND = 6;
+public class MessageType {
+	public static final String ASSIGN_ACTIVE_PLAYER = "ASSIGN_ACTIVE_PLAYER";
+	public static final String ASSIGN_PLAYER = "ASSIGN_PLAYER";
+	public static final String ELIMINATE_PLAYER = "ELIMINATE_PLAYER";
+	public static final String END_ROUND = "END_ROUND";
+	public static final String PLAYER_ROLL = "PLAYER_ROLL";
+	public static final String SPAWN_ITEM = "SPAWN_ITEM";
+	public static final String START_ROUND = "START_ROUND";
 }
