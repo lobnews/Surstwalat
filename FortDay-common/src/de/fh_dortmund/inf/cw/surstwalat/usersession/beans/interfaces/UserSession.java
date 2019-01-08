9629d21a5543b8442d7f9974bf96da459f4fc76d
Damysterio <daniel.buschmann002@stud.fh-dortmund.de>
2019-01-08 15:27:28 +0100

    Added the Local and Remote Interfaces for the UserSession

---
+++ FortDay-common/src/de/fh_dortmund/inf/cw/surstwalat/usersession/beans/interfaces/UserSession.java
@@ -0,0 +1,11 @@
+package de.fh_dortmund.inf.cw.surstwalat.usersession.beans.interfaces;
+
+
+/**
+ * @author Daniel Buschmann
+ *
+ */
+public interface UserSession{
+
+
+}
---
+++ FortDay-common/src/de/fh_dortmund/inf/cw/surstwalat/usersession/beans/interfaces/UserSessionRemote.java
@@ -0,0 +1,12 @@
+package de.fh_dortmund.inf.cw.surstwalat.usersession.beans.interfaces;
+
+import javax.ejb.Remote;
+
+/**
+ * @author Daniel Buschmann
+ *
+ */
+Remote
+public interface UserSessionLocal extends UserSession{
+
+}
