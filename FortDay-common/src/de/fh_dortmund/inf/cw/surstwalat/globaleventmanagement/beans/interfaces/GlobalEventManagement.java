29800399b03ff0214a865268e8094905f045016e
Rebekka Michel <rebekka.michel001@stud.fh-dortmund.de>
2019-01-13 21:40:30 +0100

    GlobalEventManagement Anfang
    GlobalEventManagement:
    - Bean angelegt um Nachrichten zu empfangen und zu verarbeiten
    -Bean angelegt um Nachrichten zu verschicken
    -zugehörige Message Types hinzugefügt (UPDATE_ZONE, TRIGGER_AIRDROP, TRIGGER_STARTING_ITEMS und TRIGGER_DAMAGE)
    -PropertyTypes zur Nachrichtenverarbeitung hinzugefügt (Daten zur Zone, Schaden und ID der betroffenen Spielfigur)

---
+++ FortDay-common/src/de/fh_dortmund/inf/cw/surstwalat/globaleventmanagement/beans/interfaces/GlobalEventManagement.java
@@ -0,0 +1,13 @@
+package de.fh_dortmund.inf.cw.surstwalat.globaleventmanagement.beans.interfaces;
+
+/**
+ * 
+ * @author Rebekka Michel
+ */
+
+public interface GlobalEventManagement {
+	public void updateZone(int gameId, int currentZoneBegin, int currentZoneEnd, int nextZoneBegin, int nextZoneEnd, int damage);
+	public void triggerAirdrop(int gameId);
+	public void triggerStartingItems(int gameId);
+	public void triggerDamage(int gameId, int playerNo, int characterId, int damage);
+}
