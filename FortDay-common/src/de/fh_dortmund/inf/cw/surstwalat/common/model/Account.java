31717686b0820c42f1cffc4fa2f2061c52246584
ExaShox <exashox@gmail.com>
2019-01-13 19:36:43 +0100

    Post-Merge-Fix: Account model
    ~ username -> name
    - id setter/getter duplicate

--- FortDay-common/src/de/fh_dortmund/inf/cw/surstwalat/common/model/Account.java
+++ FortDay-common/src/de/fh_dortmund/inf/cw/surstwalat/common/model/Account.java
@@ -17,32 +17,32 @@ import javax.persistence.Table;
  *
  * User account
  */
-@Table(name="Account")
+@Table(name = "Account")
 @Entity
 @NamedQueries({
-	@NamedQuery(name="Account.getById", query="SELECT a FROM Account a WHERE a.id = :id"),
-	@NamedQuery(name="Account.getInLobby", query="SELECT a FROM Account a WHERE a.inLobby = true"),
+        @NamedQuery(name = "Account.getById", query = "SELECT a FROM Account a WHERE a.id = :id"),
+        @NamedQuery(name = "Account.getInLobby", query = "SELECT a FROM Account a WHERE a.inLobby = true")
 })
-public class Account implements Serializable{
+public class Account implements Serializable {
 
-	@Id
-	@GeneratedValue
-	@Column(name="id")
-	private int id;
-	@Column(name="name")
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
     private String name;
-	@Column(name="email")
+    @Column(name = "email")
     private String email;
-	@Column(name="password")
+    @Column(name = "password")
     private String password;
-	@Column(name="inLobby")
+    @Column(name = "inLobby")
     private boolean inLobby;
 
     /**
      * Default constructor
      */
     public Account() {
-    	inLobby = false;
+        inLobby = false;
     }
 
     /**
@@ -101,63 +101,65 @@ public class Account implements Serializable{
         this.password = password;
     }
 
-    @Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + id;
-		result = prime * result + (inLobby ? 1231 : 1237);
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
-		result = prime * result + ((username == null) ? 0 : username.hashCode());
-		return result;
-	}
+    public boolean isInLobby() {
+        return inLobby;
+    }
+
+    public void setInLobby(boolean inLobby) {
+        this.inLobby = inLobby;
+    }
 
     @Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Account other = (Account) obj;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (id != other.id)
-			return false;
-		if (inLobby != other.inLobby)
-			return false;
-		if (password == null) {
-			if (other.password != null)
-				return false;
-		} else if (!password.equals(other.password))
-			return false;
-		if (username == null) {
-			if (other.username != null)
-				return false;
-		} else if (!username.equals(other.username))
-			return false;
-		return true;
-	}
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + id;
+        result = prime * result + (inLobby ? 1231 : 1237);
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public boolean isInLobby() {
-		return inLobby;
-	}
-
-	public void setInLobby(boolean inLobby) {
-		this.inLobby = inLobby;
-	}
-
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Account other = (Account) obj;
+        if (email == null) {
+            if (other.email != null) {
+                return false;
+            }
+        } else if (!email.equals(other.email)) {
+            return false;
+        }
+        if (id != other.id) {
+            return false;
+        }
+        if (inLobby != other.inLobby) {
+            return false;
+        }
+        if (password == null) {
+            if (other.password != null) {
+                return false;
+            }
+        } else if (!password.equals(other.password)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
 }
