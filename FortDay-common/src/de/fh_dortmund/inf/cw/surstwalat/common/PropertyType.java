f8c4c0b95d3281607986d577b1fd9a039613805f
Niklas Sp <ni.sp.mi2@gmail.com>
2019-01-21 18:42:04 +0100

    Removed unused properties

--- FortDay-common/src/de/fh_dortmund/inf/cw/surstwalat/common/PropertyType.java
+++ FortDay-common/src/de/fh_dortmund/inf/cw/surstwalat/common/PropertyType.java
@@ -12,10 +12,6 @@ public interface PropertyType {
 	public static final String PLAYER_NO = "PLAYER_NO";
 	public static final String ACTION_TYPE = "ACTION_TYPE";
 	public static final String USER_ID = "USER_ID";
-	public static final String USER1_ID = "USER1_ID";
-	public static final String USER2_ID = "USER2_ID";
-	public static final String USER3_ID = "USER3_ID";
-	public static final String USER4_ID = "USER4_ID";
 	public static final String LOBBY_NAME = "LOBBY_NAME";
 	public static final String GAME_FIELDSIZE = "GAME_FIELDSIZE";
 	public static final String CURRENT_ZONE_BEGIN = "CURRENT_ZONE_BEGIN";
