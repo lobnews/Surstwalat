6f0920eed258b4d0be46a57ddcb8400579177cb1
Z0mbiel0ne <schlotz23marcel@gmail.com>
2019-01-25 18:25:28 +0100

    Ausgehende Nachrichten
    Signed-off-by: Lars Borisek <lars.borisek002@stud.fh-dortmund.de>

--- FortDay-KI/ejbModule/de/fh_dortmund/cw/surstwalat/ejb/ki/KiBean.java
+++ FortDay-KI/ejbModule/de/fh_dortmund/cw/surstwalat/ejb/ki/KiBean.java
@@ -4,19 +4,25 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.ejb.Stateless;
 
+import de.fh_dortmund.cw.surstwalat.ejb.ki.OutgoingEventHelperBean;
 import de.fh_dortmund.inf.cw.surstwalat.common.model.Item;
 import de.fh_dortmund.inf.cw.surstwalat.ki.beans.interfaces.KiRemote;
 import de.fh_dortmund.inf.cw.surstwalat.usersession.beans.interfaces.UserSession;
 
+@Stateless
 public class KiBean implements KiRemote {
 	
 	@EJB
 	private UserSession session;
 	
+	@EJB
+	private OutgoingEventHelperBean sender;
+	
 	private int gameid;
 	
-	private int playerid;
+	private int player_no;
 	
 	private List<Item> inventory;
 	
@@ -32,7 +38,17 @@ public class KiBean implements KiRemote {
 
 	@Override
 	public void makeTurn() {
+		sender.sendInventoryRequest(gameid, player_no);
+		this.setInventory(getInventory());
 		
+		if(!inventory.isEmpty())
+		{
+			
+		}
+		else
+		{
+			
+		}
 	}
 
 	public int getGameid() {
@@ -44,11 +60,11 @@ public class KiBean implements KiRemote {
 	}
 
 	public int getPlayerid() {
-		return playerid;
+		return player_no;
 	}
 
 	public void setPlayerid(int playerid) {
-		this.playerid = playerid;
+		this.player_no = playerid;
 	}
 
 	public void setInventory(List<Item> inventory) {
