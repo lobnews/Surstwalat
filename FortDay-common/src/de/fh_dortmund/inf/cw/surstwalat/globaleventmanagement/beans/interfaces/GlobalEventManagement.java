9aa6e0acbf1a8f8befe59c496bbf808991554be3
Rebekka Michel <rebekka.michel001@stud.fh-dortmund.de>
2019-01-29 14:17:12 +0100

    Updated "Zone" to "DamageZone" in the persistence.xml files
    "Zone" wurde in "DamageZone" umbenannt, zudem wurde eine Variablenname im GlobalEventManagement angepasst

--- FortDay-common/src/de/fh_dortmund/inf/cw/surstwalat/globaleventmanagement/beans/interfaces/GlobalEventManagement.java
+++ FortDay-common/src/de/fh_dortmund/inf/cw/surstwalat/globaleventmanagement/beans/interfaces/GlobalEventManagement.java
@@ -10,7 +10,7 @@ import de.fh_dortmund.inf.cw.surstwalat.common.model.Token;
  */
 
 public interface GlobalEventManagement {
-	public void updateZone(int gameId, int damage);
+	public void updateZone(int gameId, int roundNo);
 	public void triggerAirdrop(int gameId);
 	public void triggerStartingItems(int gameId);
 	public void triggerDamage(int gameId, List<Token> token);
